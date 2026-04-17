f56835283197d8b77288d9a319bf6d5cafc526f5
Gustavo Wallace <Clasherbr@gmail.com>
2026-04-17 17:18:04 -0300

    Item and Player initial classes

--- src/com/lemon_legacy/models/Item.java
+++ src/com/lemon_legacy/models/Item.java
@@ -1,5 +1,26 @@
 package com.lemon_legacy.models;
 
 public class Item {
-    
+
+    private String name;
+    private String type;
+    private int value;
+    private int heal;
+    private int mana;
+    private int bonusAtack;
+    private int bonusHealth;
+    private int bonusDefense;
+
+    public Item(String name, String type, int value, int heal, int mana, int bonusAtack, int bonusHealth, int bonusDefense) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+        this.heal = heal;
+        this.mana = mana;
+        this.bonusAtack = bonusAtack;
+        this.bonusHealth = bonusHealth;
+        this.bonusDefense = bonusDefense;
+    }
+
+
 }
--- src/com/lemon_legacy/models/Player.java
+++ src/com/lemon_legacy/models/Player.java
@@ -3,52 +3,28 @@ package com.lemon_legacy.models;
 public class Player {
 
     private String name;
-    private static int role;
-    private static int hpMax;
-    private static int hp;
-    private static int defense;
-    private static int strength;
-    private static int agility;
-    private static int magic;
+    private int level = 1;
+    private int xp = 0;
+    private int xpToLevel = 40;
 
-    public Player(String name, int role){
+    private int maxHealth = 100;
+    private int Health = 1000;
+
+    private int maxMana = 40;
+    private int mana = 40;
+
+    private int atack = 12;
+    private int defense = 3;
+    private int gold = 30;
+
+    private int x = 0;
+    private int y = 0;
+
+    public Player(String name) {
         this.name = name;
-        this.role = role;
     }
 
-    public void takeDamage(int value) {
-        int finalDamage = value - defense;
 
-        if (finalDamage < 0) {
-            finalDamage = 0;
-        }
 
-        hp -= finalDamage;
-    }
-
-    public void heal(int value) {
-        hp += value;
-
-        if (hp > hpMax) {
-            hp = hpMax;
-        }
-    }
-
-    public void showStats() {
-        System.out.println();
-        System.out.println("Name: " + name);
-        System.out.print("Class: ");
-        System.out.println((role == 1) ? "Warrior" : (role == 2) ? "Mage" : "Rogue");
-        System.out.println();
-        System.out.println("== Defensive stats ==");
-        System.out.println("Max HP: " + hpMax);
-        System.out.println("Current HP: " + hp);
-        System.out.println("Defense: " + defense);
-        System.out.println();
-        System.out.println("== Ofensive stats ==");
-        System.out.println("Strength: " + strength);
-        System.out.println("Agility: "+ agility);
-        System.out.println("Magic: " + magic);
-    }
     
 }

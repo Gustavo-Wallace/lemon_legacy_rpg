0d61e80f59efb76c1d521c511b660244d3fe5a20
Gustavo Wallace <Clasherbr@gmail.com>
2026-04-23 13:13:13 -0300

    Enemy class

--- src/com/lemon_legacy/models/Enemy.java
+++ src/com/lemon_legacy/models/Enemy.java
@@ -1,5 +1,65 @@
 package com.lemon_legacy.models;
 
 public class Enemy {
+
+    private String name;
+    private int maxHealth;
+    private int health;
+    private int attack;
+    private int defense;
+    private int rewardGold;
+    private int rewardXp;
+
+    public Enemy (String name, int maxHealth, int health, int attack, int defense, int rewardGold, int rewardXp) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+        this.rewardGold = rewardGold;
+        this.rewardXp = rewardXp;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public void receiveDamage(int damage) {
+        int finalDamage = damage - defense;
+
+        if (finalDamage < 1) {
+            finalDamage = 1;
+        }
+
+        health -= finalDamage;
+
+        if (health < 0) {
+            health = 0;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getRewardGold() {
+        return rewardGold;
+    }
+
+    public int getRewardXp() {
+        return rewardXp;
+    }
     
 }

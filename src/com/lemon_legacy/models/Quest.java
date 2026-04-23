340dd76700c5a4811b0e28973683bf3548b4df95
Gustavo Wallace <Clasherbr@gmail.com>
2026-04-23 13:24:37 -0300

    Quest class

--- src/com/lemon_legacy/models/Quest.java
+++ src/com/lemon_legacy/models/Quest.java
@@ -1,5 +1,38 @@
 package com.lemon_legacy.models;
 
 public class Quest {
+
+    private String name;
+    private String targetEnemy;
+    private int requiredAmount;
+    private int currentProgress;
+    private int rewardGold;
+    private int rewardXp;
+    private boolean completed;
+
+    public Quest(String name, String targetEnemy, int requiredAmount, int rewardGold, int rewardXp) {
+        this.name = name;
+        this.targetEnemy = targetEnemy;
+        this.requiredAmount = requiredAmount;
+        this.currentProgress = 0;
+        this.rewardGold = rewardGold;
+        this.rewardXp = rewardXp;
+        this.completed = false;
+    }
+
+    public void registerKill(String enemyName) {
+        if (completed) {
+            return;
+        }
+
+        if (targetEnemy.equals(enemyName)) {
+            currentProgress++;
+            
+            if (currentProgress >= requiredAmount) {
+                completed = true;
+            }
+        }
+
+    }
     
 }

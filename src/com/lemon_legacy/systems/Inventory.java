23c3b0c7dd021cccf709dd0351f17f2436b5e36b
Gustavo Wallace <Clasherbr@gmail.com>
2026-05-06 13:11:12 -0300

    inventory improvement

--- src/com/lemon_legacy/systems/Inventory.java
+++ src/com/lemon_legacy/systems/Inventory.java
@@ -20,7 +20,27 @@ public class Inventory {
         for (int i = 0; i < inventory.size(); i++) {
             Item item = inventory.get(i);
 
-            System.out.println((i + 1) + " - " + item.getName() + " (" + item.getType() + ")");
+            System.out.println((i + 1) + " - " + item.getName() + " (" + item.getType() + ") ");
+
+            if (item.getHeal() > 0) {
+                System.out.println("| Heal: " + item.getHeal());
+            }
+
+            if (item.getMana() > 0) {
+                System.out.println("| Mana: " + item.getMana());
+            }
+
+            if (item.getBonusAttack() > 0) {
+                System.out.println("| Attack: " + item.getBonusAttack());
+            }
+
+            if (item.getBonusDefense() > 0) {
+                System.out.println("| Defense: " + item.getBonusDefense());
+            }
+
+            if (item.getBonusHealth() > 0) {
+                System.out.println("| Health: " + item.getBonusHealth());
+            }
         }
 
         System.out.println();

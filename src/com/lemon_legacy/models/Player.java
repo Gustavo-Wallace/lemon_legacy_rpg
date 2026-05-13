b24080984c6c6118f2403806b6e39e696aed8b1e
Gustavo Wallace <Clasherbr@gmail.com>
2026-05-13 14:55:24 -0300

    random adventure exploration events

--- src/com/lemon_legacy/models/Player.java
+++ src/com/lemon_legacy/models/Player.java
@@ -182,6 +182,8 @@ public class Player {
 
             health = getTotalMaxHealth();
             mana = maxMana;
+
+            System.out.println("You've leveled up!");
             
         }
     }
--- src/com/lemon_legacy/systems/Adventure.java
+++ src/com/lemon_legacy/systems/Adventure.java
@@ -3,11 +3,14 @@ package com.lemon_legacy.systems;
 import com.lemon_legacy.Utils;
 import com.lemon_legacy.factories.EnemyFactory;
 import com.lemon_legacy.models.Enemy;
+import com.lemon_legacy.models.Item;
 import com.lemon_legacy.models.Player;
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class Adventure {
+
+    private static final Random random = new Random();
 
     public static void openAdventureMenu(Player player, Scanner sc) {
         boolean running = true;
@@ -28,7 +31,7 @@ public class Adventure {
                     break;
 
                 case 2:
-                    exploreArea();
+                    exploreArea(player, sc);
                     break;
 
                 case 0:
@@ -44,7 +47,64 @@ public class Adventure {
         }
     }
 
-    private static void exploreArea() {
-        System.out.println("You explore the area, but nothing happens yet.");
+    private static void exploreArea(Player player, Scanner sc) {
+        int event = random.nextInt(5);
+
+        switch (event) {
+            case 0:
+                findGold(player);
+                break;
+
+            case 1:
+                findHealthPotion(player);
+                break;
+
+            case 2:
+                findManaPotion(player);
+                break;
+
+            case 3:
+                findEnemy(player, sc);
+                break;
+
+            case 4:
+                System.out.println("You explore the area, but nothing happens.");
+                break;
+
+            default:
+                System.out.println("You continue walking...");
+                break;
+        }
+    }
+
+    private static void findGold(Player player) {
+        int goldFound = random.nextInt(11) + 5;
+
+        player.addGold(goldFound);
+
+        System.out.println("You found " + goldFound + " gold!");
+    }
+
+    private static void findHealthPotion(Player player) {
+        Item potion = new Item("Health Potion", "consumable", 10, 30, 0, 0, 0, 0);
+
+        player.addItem(potion);
+
+        System.out.println("You found a Health Potion!");
+    }
+
+    private static void findManaPotion(Player player) {
+        Item potion = new Item("Mana Potion", "consumable", 12, 0, 20, 0, 0, 0);
+
+        player.addItem(potion);
+
+        System.out.println("You found a Mana Potion!");
+    }
+
+    private static void findEnemy(Player player, Scanner sc) {
+        System.out.println("A monster appears!");
+
+        Enemy enemy = EnemyFactory.createRandomEnemy(player.getLevel());
+        Combat.startBattle(player, enemy, sc);
     }
 }
